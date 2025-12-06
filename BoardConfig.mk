c6b755b252fdbcf101b1d6c96fce113fbb920082
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-06 21:02:36 +0000

    Rebrand m14
    Change-Id: Ibd086a2589159b8ba7d03420cd7004b9894b16fa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a05s
+DEVICE_PATH := device/samsung/m14
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
